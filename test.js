4b7d9ad78255766e88e4be825303d95844d267f7
skepticfx <ahamed.nafeez@gmail.com>
2015-01-06 00:52:23 +0530

    travis: sudo 12

--- test.js
+++ test.js
@@ -1,15 +1,11 @@
 var pcap = require("pcap"),
-    pcap_session = pcap.createSession("", "tcp"),
+    pcap_session = pcap.createSession("", ""),
     matcher = /safari/i;
 
 console.log("Listening on " + pcap_session.device_name);
 
 pcap_session.on('packet', function (raw_packet) {
-    var packet = pcap.decode.packet(raw_packet),
-        data = packet.link.ip.tcp.data;
-
-    if (data && matcher.test(data.toString())) {
-        console.log(pcap.print.packet(packet));
-        console.log(data.toString());
-    }
+    var packet = pcap.decode.packet(raw_packet);
+        //data = packet.link.ip.tcp.data;
+      console.log(raw_packet);
 });

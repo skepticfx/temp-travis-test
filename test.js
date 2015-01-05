f437f6d1e2457abb0c8c188d735a2548cedcac5f
skepticfx <ahamed.nafeez@gmail.com>
2015-01-06 01:57:11 +0530

    travis: sudo 123j

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var pcap = require("pcap"),
-    pcap_session = pcap.createSession("lo", ""),
+    pcap_session = pcap.createSession("lo", "");
 
 
 console.log("Listening on " + pcap_session.device_name);

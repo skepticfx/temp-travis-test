fa9bbe341e8daa5a950195cc8f5cd711693dfc53
skepticfx <ahamed.nafeez@gmail.com>
2015-01-12 21:22:13 -0800

    travis: looks good 6

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var pcap = require("pcap");
 var os = require('os');
 var should =require('should');
+var assert = require('assert');
 
 function getValidInterface(){
   var list = Object.keys(os.getNetworkInterfaces());
@@ -10,15 +11,11 @@ function getValidInterface(){
   }
 }
 
-describe('test capture interface', function(){
-  var pcap_session = pcap.createSession(getValidInterface(), "");
-  console.log("Listening on " + pcap_session.device_name);
+var pcap_session = pcap.createSession(getValidInterface(), "");
+console.log("Listening on " + pcap_session.device_name);
 
-  it('capture one packet', function(done){
-    pcap_session.once('packet', function (raw_packet) {
-      var packet = pcap.decode.packet(raw_packet);
-      done();
-    })
-  })
-
+pcap_session.once('packet', function (raw_packet) {
+  var packet = pcap.decode.packet(raw_packet);
+  assert(true);
+  process.nextTick(process.exit);
 })
